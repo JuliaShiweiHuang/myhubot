3560fd4c223ef6bcc583506c2b5e616e6941cce3
Shiwei Huang <shiwei.julia.huang@gmail.com>
2016-07-13 21:39:21 -0400

    commit changes

--- scripts/test.js
+++ scripts/test.js
@@ -20,5 +20,28 @@ module.exports = function(robot) {
 		return res.send("NO NO NO. I need a match account.");
 	});
 
+
+
+	bot.respond(/what is your favorite (.*)/, function(msg) {
+	var fav;
+	fav = msg.match[1];
+	console.log(fav);
+		switch (fav) {
+			case "food":
+				return msg.reply("I'm a robot--I don't eat food!");
+				break;
+			case "band":
+				return msg.reply("It's gotta be Daft Punk!");
+				break;
+			case "programming language":
+				return msg.reply("Javascript, of course!");
+				break;
+			default:
+				return msg.reply("I don't have a favorite " + fav + ". What's yours?");
+		}
+	});
+
+
+
 }
 
