cc51de4f0451a3e82a769c4240edc397c7a6f69f
Shiwei Huang <shiwei.julia.huang@gmail.com>
2016-07-13 21:33:28 -0400

    commit changes

--- scripts/test.js
+++ scripts/test.js
@@ -1,4 +1,4 @@
-module.export = function(robot) {
+module.exports = function(robot) {
 
 	robot.respond(/What's your favorite food my cherry?/, function(res) {
 		return res.send("I'm a robot--I suggest you going to a bar.");
@@ -13,14 +13,12 @@ module.export = function(robot) {
 	});
 
 	robot.respond(/What's your favorite food?/, function(res) {
-	return res.send("I like apple");
+		return res.send("I like apple");
+	});
 
 	robot.respond(/Do you have a girlfriend?/, function(res) {
-	return res.send("NO NO NO. I need a match account.");
+		return res.send("NO NO NO. I need a match account.");
 	});
 
 }
 
-
-
-});
