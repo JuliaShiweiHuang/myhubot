08b0a7af629a3bc9a1a620355e58e8f5860c408d
Shiwei Huang <shiwei.julia.huang@gmail.com>
2016-07-18 19:35:43 -0400

    add changes

--- scripts/regularTest.js
+++ scripts/regularTest.js
@@ -18,3 +18,21 @@ if (minutes < 10) {
 }
 
 console.log("It's " + hours + ":" + minutes);
+
+
+var addressHolder = "";
+
+
+var streetNumber = [34, 215, 18, 1, 289];
+var streetName = ["A Street", "Congress Street", "St. Stephen Street", "Willet Street", "East Main Street", "Westfield Dr"];
+var cityName = ["Itica", "Boston", "Dorchester", "Quincy", "Wollaston", "Kingston", "San Francisco", "Los Angeles", "New York", "Cambridge"];
+var stateName = ["NY", "NH", "VT", "CA"];
+var zipCode = [02210, 02131, 17938, 20086];
+
+	var randomStreetNum = streetNumber[Math.floor(Math.random() * streetNumber.length)];
+	var randomStreetName = streetName[Math.floor(Math.random() * streetName.length)];
+	var randomCityName = cityName[Math.floor(Math.random() * cityName.length)];
+	var randomStateName = stateName[Math.floor(Math.random() * stateName.length)];
+	var randomZipCode = zipCode[Math.floor(Math.random() * zipCode.length)];
+	addressHolder = addressHolder + randomStreetNum + " " + randomStreetName + ", " + randomCityName + " " + randomStateName + ", " + randomZipCode;
+console.log(addressHolder);
--- scripts/test.js
+++ scripts/test.js
@@ -70,6 +70,22 @@ module.exports = function(robot) {
 
 	});
 
+	robot.respond(/Generate a random address!/, function(res) {
+		var streetNumber = [34, 215, 18, 1, 289];
+		var streetName = ["A Street", "Congress Street", "St. Stephen Street", "Willet Street", "East Main Street", "Westfield Dr"];
+		var cityName = ["Itica", "Boston", "Dorchester", "Quincy", "Wollaston", "Kingston", "San Francisco", "Los Angeles", "New York", "Cambridge"];
+		var stateName = ["NY", "NH", "VT", "CA"];
+		var zipCode = [02210, 02131, 17938, 20086];
+
+		var randomStreetNum = streetNumber[Math.floor(Math.random() * streetNumber.length)];
+		var randomStreetName = streetName[Math.floor(Math.random() * streetName.length)];
+		var randomCityName = cityName[Math.floor(Math.random() * cityName.length)];
+		var randomStateName = stateName[Math.floor(Math.random() * stateName.length)];
+		var randomZipCode = zipCode[Math.floor(Math.random() * zipCode.length)];
+		
+		return res.send(addressHolder = addressHolder + randomStreetNum + " " + randomStreetName + ", " + randomCityName + " " + randomStateName + ", " + randomZipCode);
+
+	});
 
 
 }
